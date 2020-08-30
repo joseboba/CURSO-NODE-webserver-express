9f943c08f84f207fa801cf218bf401e37ddf03bb
Jose Enrique <joseboba@pop-os.localdomain>
2020-08-30 15:59:26 -0600

    Fixed port error

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 const hbs = require('hbs')
 require('./hbs/helpers');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+  
 
 app.use( express.static(__dirname + '/public') );
 
