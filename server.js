7669c4d80d01b1e391754048209017ede0dcd304
Jose Enrique <joseboba@pop-os.localdomain>
2020-08-30 15:47:15 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000
+
+app.use( express.static(__dirname + '/public') );
+
+//Express HBS engine
+hbs.registerPartials( __dirname + '/views/parciales' )
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    
+    res.render('home', {
+        nombre: 'Jose Enrique',
+    })
+})
+app.get('/about', (req, res) => {
+    
+    res.render('about')
+})
+app.listen(3000, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`)
+})
