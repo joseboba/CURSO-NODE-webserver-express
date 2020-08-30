fe6ef4723577072d4f2edd9c87960f752dff3a6d
Jose Enrique <joseboba@pop-os.localdomain>
2020-08-30 16:01:32 -0600

    Port fixed

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get('/about', (req, res) => {
     
     res.render('about')
 })
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`)
 })
